2abcdd64106e7f681bea0959b71b29192455d21d
huzefa <huzefasr9@gmail.com>
2019-06-09 16:43:42 +0530

    with rect

--- scan_text.py
+++ scan_text.py
@@ -6,9 +6,9 @@ while True:
     ret,frame = cam.read()
     key = cv2.waitKey(1)
     flip = cv2.flip(frame,1)
+    cv2.rectangle(flip,(50,50),(200,200),(0,0,255),1)
     cv2.imshow("frame",flip)
-
-    if key == ord('s'):
+    if key == ord('x'):
         break
 
 cam.release()
