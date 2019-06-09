23278a9d280546a43d60621d993daf4711a75504
Murtuza Bagasrawala <murtuzabagasrawala@gmail.com>
2019-06-09 18:32:57 +0530

    Model changes

--- model.py
+++ model.py
@@ -7,21 +7,24 @@ import h5py
 (train_x,train_y),(test_x,test_y) = mnist.load_data()
 X = normalize(train_x,1)
 model = Sequential()
-print(X.shape())
+print(train_x.shape)
 #INPUT LAYER
-model.add(Conv2D(32,(5,5),input_shape=X.shape[1:]),activation='relu')
+model.add(Conv2D(32,(5,5),input_shape=(28,28,1),activation='relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
 #HIDDEN LAYER 1
 model.add(Conv2D(64,(5,5),activation='relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
 #PREPARING DATA FOR OUTPUT LAYER
 model.add(Flatten())
 #OUTPUT layer
-model.add(Dense(output))
+model.add(Dense(10))
 model.add(Activation('sigmoid'))
 
 model.compile(loss="sparse_categorical_crossentropy",
              optimizer="adam",
              metrics=['accuracy']
              )
-
-model.fit(train_x,train_y,test_x,test_y,epochs=1)
+train_x = train_x.reshape(train_x.shape[0],28,28,1)
+test_x = test_x.reshape(test_x.shape[0],28,28,1)
+model.fit(train_x,train_y,epochs=6,validation_data=(test_x,test_y),batch_size=30)
 model.save('mnist-test.h5')
