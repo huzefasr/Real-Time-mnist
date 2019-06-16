05c3bf00a5532bb114cbab2739bd3feee268b99d
Murtuza Bagasrawala <murtuzabagasrawala@gmail.com>
2019-06-16 13:44:22 +0530

    Normalizing train and test data, Adding dropout,changed dense layer activationn to softmax, Epochs changed

--- model.py
+++ model.py
@@ -1,16 +1,18 @@
 from keras.datasets import mnist
 from keras.utils import normalize
-from keras.layers import Dense, Activation, Conv2D, Flatten, MaxPooling2D
+from keras.layers import Dense, Activation, Conv2D, Flatten, MaxPooling2D, Dropout
 from keras.models import Sequential
 import h5py
 
 (train_x,train_y),(test_x,test_y) = mnist.load_data()
-X = normalize(train_x,1)
+train_x = normalize(train_x,1)
+test_x = normalize(test_x,1)
 model = Sequential()
 print(train_x.shape)
 #INPUT LAYER
 model.add(Conv2D(32,(5,5),input_shape=(28,28,1),activation='relu'))
 model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.25))
 #HIDDEN LAYER 1
 model.add(Conv2D(64,(5,5),activation='relu'))
 model.add(MaxPooling2D(pool_size=(2,2)))
@@ -18,7 +20,7 @@ model.add(MaxPooling2D(pool_size=(2,2)))
 model.add(Flatten())
 #OUTPUT layer
 model.add(Dense(10))
-model.add(Activation('sigmoid'))
+model.add(Activation('softmax'))
 
 model.compile(loss="sparse_categorical_crossentropy",
              optimizer="adam",
@@ -26,5 +28,5 @@ model.compile(loss="sparse_categorical_crossentropy",
              )
 train_x = train_x.reshape(train_x.shape[0],28,28,1)
 test_x = test_x.reshape(test_x.shape[0],28,28,1)
-model.fit(train_x,train_y,epochs=6,validation_data=(test_x,test_y),batch_size=30)
+model.fit(train_x,train_y,epochs=3,validation_data=(test_x,test_y),batch_size=30)
 model.save('mnist-test.h5')
--- scan_text.py
+++ scan_text.py
@@ -1,16 +1,18 @@
 import cv2
 import numpy
-import matplotlib
 import h5py
 import keras
 from keras.models import load_model
 import numpy as np
+model = load_model('keras-mnist-model.h5')
 
 def infer(crop):
-    model = load_model('mnist-test.h5')
-    image = np.resize(crop,(28,28,1))
+    image = np.reshape(crop,(-1,28,28,1))
     result = model.predict(image)
+    result = result[0]
+    #result = list(map(int,result))
     print(result)
+    
 
 
 ###CODE
@@ -23,13 +25,20 @@ while True:
     flip = cv2.flip(frame,1)
     cv2.rectangle(flip,(50,50),(250,250),(0,0,255),1)
     roi = flip[60:250,60:250]  ## size taken is 190x190
-    #thresh = cv2.adaptiveThreshold(roi,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,2)
     roi_gray = cv2.cvtColor(roi,cv2.COLOR_BGR2GRAY)
+    # thresh = cv2.adaptiveThreshold(roi,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,2)
     _,thresh = cv2.threshold(roi_gray,120,255,cv2.THRESH_BINARY)
     crop = cv2.resize(thresh,(28,28))
-    text = infer(crop)
+    '''
+    crop = cv2.imread('./mnist_dream_10.png',0)
+    _,thresh2 = cv2.threshold(crop,120,255,cv2.THRESH_BINARY)
+    crop = cv2.resize(crop,(28,28))
+    cv2.imshow("crop",crop)
+    '''
+    infer(crop)
     cv2.imshow("frame",roi)
-    cv2.imshow("thresh",thresh)
+    cv2.imshow("roi",roi_gray)
+
     if key == ord('x'):
         break
 
